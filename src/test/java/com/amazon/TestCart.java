22dc246b69ecafb8708e32445ba6e80cbb62c42e
nishNaik17 <67681644+nishNaik17@users.noreply.github.com>
2023-10-27 19:26:37 +0530

    Create TestCart.java
    Cart testing

---
+++ src/test/java/com/amazon/TestCart.java
@@ -0,0 +1,20 @@
+package test.java.com.amazon;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import main.java.com.browser.CallBrowser;
+
+public class TestCart extends CallBrowser{
+	
+	
+	@Test
+	public void login() throws IOException, InterruptedException {
+		driver = browser("https://www.amazon.in/");
+		Thread.sleep(3000);
+		driver.close();
+		driver.quit();
+
+	}
+}
